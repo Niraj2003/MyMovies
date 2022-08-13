fdfeecdf495c4bbc4c0443964b6ef7f7713436a1
Niraj Amrutkar <>
2022-08-13 10:57:39 +0530

    gh pages updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Top from './components/Top';
 import TableData from './components/Table';
