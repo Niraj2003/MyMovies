243f38058f4f454cb7f9cd97367e14e1a9545e51
Niraj Amrutkar <>
2022-08-30 16:36:07 +0530

    Updated size of text

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ function About() {
   return (
     <div id='about'>
         <dl className='mx-5' style={{ display: 'block', marginTop: '1em',marginBottom: '1em',marginLeft: 0,marginRight: 0}}>
-            <dt>About Project :</dt>
-            <dd> &ensp;&ensp; This is the app of some collected movies and web series, in that user can add movies in given 
+            <dt  style={{ fontSize: 20}}>About Project :</dt>
+            <dd style={{ fontSize: 18 }}> &ensp;&ensp; This is the app of some collected movies and web series, in that user can add movies in given 
                 movie-list and user also see brief about movie by clicking on moviename and can view rating of 
                 that movie on famous site IMDB</dd>
-            <dt>Technologies Used :</dt>
-            <dd> &ensp;&ensp; ReactJS, Html, CSS, JS, Bootstrap</dd>
+            <dt  style={{ fontSize: 20 }}>Technologies Used :</dt>
+            <dd  style={{ fontSize: 18 }} > &ensp;&ensp; ReactJS, Html, CSS, JS, Bootstrap</dd>
         </dl>
     </div>
   )
